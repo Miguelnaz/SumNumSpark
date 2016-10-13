1a571e750ae89d647ba87ab18256ee3eabcd124a
nazarage@hotmail.com <cloudera@clouderaVM091.(none)>
2016-10-13 18:34:50 +0200

    First commit

---
+++ src/main/java/org/MN/PruebaGit/App.java
@@ -0,0 +1,13 @@
+package org.MN.PruebaGit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

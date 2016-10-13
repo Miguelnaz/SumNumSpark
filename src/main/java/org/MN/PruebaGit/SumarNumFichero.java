50c92f9dfdd4719e67ae580ab99e36fb8e1ebf58
nazarage@hotmail.com <cloudera@clouderaVM091.(none)>
2016-10-13 20:25:11 +0200

    Modify SNF

--- src/main/java/org/MN/PruebaGit/SumarNumFichero.java
+++ src/main/java/org/MN/PruebaGit/SumarNumFichero.java
@@ -22,11 +22,22 @@ public class SumarNumFichero
     	
     	JavaSparkContext context = new JavaSparkContext(conf);
     	
+    	/*
+    	
     	JavaRDD<String> lines = context.textFile(args[0]);
     	
     	JavaRDD<Integer> rddOfIntegers = lines.map(s -> Integer.valueOf(s));
     	
     	int sum = rddOfIntegers.reduce( (integer1, integer2) -> (integer1+integer2));
+    	*/
+    	
+    	int sum= context
+    			.textFile(args[0])
+    			.map(s -> Integer.valueOf(s))
+    			.reduce( (integer1, integer2) -> (integer1+integer2));
+    			
+    			
+    
     	
     	
         System.out.println( "Sum is: "+sum );

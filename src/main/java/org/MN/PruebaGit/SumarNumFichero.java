4118c3374dee627510b83e0b1a7d60a5d66be772
nazarage@hotmail.com <cloudera@clouderaVM091.(none)>
2016-10-13 20:12:12 +0200

    Added with file

--- src/main/java/org/MN/PruebaGit/SumarNumFichero.java
+++ src/main/java/org/MN/PruebaGit/SumarNumFichero.java
@@ -11,7 +11,7 @@ import org.apache.spark.api.java.JavaSparkContext;
  * Hello world!
  *
  */
-public class SumarNum 
+public class SumarNumFichero
 {
     public static void main( String[] args )
     {
@@ -22,14 +22,11 @@ public class SumarNum
     	
     	JavaSparkContext context = new JavaSparkContext(conf);
     	
-    	//Step3:
+    	JavaRDD<String> lines = context.textFile(args[0]);
     	
-    	Integer[] numbers = new Integer[] {3,56,364,32,236524757,7750};
-    	List<Integer>listOfNumbers= Arrays.asList(numbers);
+    	JavaRDD<Integer> rddOfIntegers = lines.map(s -> Integer.valueOf(s));
     	
-    	JavaRDD<Integer> rddOfNumbers = context.parallelize(listOfNumbers);
-    	
-    	int sum = rddOfNumbers.reduce( (integer1, integer2) -> (integer1+integer2));
+    	int sum = rddOfIntegers.reduce( (integer1, integer2) -> (integer1+integer2));
     	
     	
         System.out.println( "Sum is: "+sum );
